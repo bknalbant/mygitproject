fc507a81a1b8a3f726671ee8d01d89190e03d704
Burak Nalbant <bk.nalbant@gmail.com>
2015-11-15 20:25:37 +0200

    this is first commit to our

---
+++ src/mygitproject/Sample.java
@@ -0,0 +1,9 @@
+package mygitproject;
+
+public class Sample {
+	private static String name="Ahmet";
+	public static void main(String[] args) {
+		System.out.println("hello my name is"+name);
+	}
+
+}
